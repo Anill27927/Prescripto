395f9c235b5b2cf02e5b22614fed06c97d6eb740
Anill27927 <anilbadithimani@gmail.com>
2026-08-03 21:28:03 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+// simple express app
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+// app config
+// Creating app instance
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// api  endpoints
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => console.log("Server Started", port));
