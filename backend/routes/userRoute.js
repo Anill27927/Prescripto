d9c7bf88046862b80291eeeb2332b5b2d2f38c54
Anill27927 <anilbadithimani@gmail.com>
2026-08-10 22:56:46 +0530

    Display Booked appointments on Website

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,6 +5,7 @@ import {
   getProfile,
   updateProfile,
   bookAppoinment,
+  listAppointment,
 } from "../controllers/userController.js";
 import authUser from "../middlewares/authUser.js";
 import upload from "../middlewares/multer.js";
@@ -21,5 +22,6 @@ userRouter.post(
   updateProfile,
 );
 userRouter.post("/book-appointment", authUser, bookAppoinment);
+userRouter.get("/appointments", authUser, listAppointment);
 
 export default userRouter;
