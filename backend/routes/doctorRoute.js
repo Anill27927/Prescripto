1cde8e09b552961733fe61e7eecb07140bd1f8c2
Anill27927 <anilbadithimani@gmail.com>
2026-08-15 01:33:43 +0530

    Display and manage appointments in Doctor panel

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { doctorList, loginDoctor } from "../controllers/doctorsController.js";
+import {
+  doctorList,
+  loginDoctor,
+  appoinmentsDoctor,
+  appointmentComplete,
+  appointmentCancel,
+} from "../controllers/doctorsController.js";
+import authDoctor from "../middlewares/authDoctor.js";
 
 const doctorRouter = express.Router();
 
 doctorRouter.get("/list", doctorList);
 doctorRouter.post("/login", loginDoctor);
+doctorRouter.get("/appointments", authDoctor, appoinmentsDoctor);
+doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete);
+doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel);
 
 export default doctorRouter;
